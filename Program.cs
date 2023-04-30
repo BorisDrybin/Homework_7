4e1acdd5f3bcf0968c679be5c0859d87def0bd2e
Boris Drybin <makoh93@gmail.com>
2023-04-30 21:09:04 +0300

    third task added

--- Program.cs
+++ Program.cs
@@ -40,6 +40,50 @@
 
 
 
+// Console.WriteLine("Введите количество строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите количество столбцов");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[,] array = new int [rows, columns];
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array[i, j] =new Random().Next(0, 10);
+//         Console.Write(array[i, j]+ " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine("Введите номер элемента строки");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите номер элемента столбца");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if ((a < rows) && (b < columns))
+//      {
+//         Console.WriteLine(array[a, b]);
+//     }
+//     else
+//     {
+//         Console.WriteLine("Такого числа в массиве нет");
+//     }
+
+
+
+// Задайте двумерный массив из целых чисел. Найдите среднее 
+// арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
 Console.WriteLine("Введите количество строк");
 int rows = Convert.ToInt32(Console.ReadLine());
 
@@ -58,17 +102,14 @@ for (int i = 0; i < array.GetLength(0); i++)
     Console.WriteLine();
 }
 
-Console.WriteLine("Введите номер элемента строки");
-int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("___________");
 
-Console.WriteLine("Введите номер элемента столбца");
-int b = Convert.ToInt32(Console.ReadLine());
-
-if ((a < rows) && (b < columns))
-     {
-        Console.WriteLine(array[a, b]);
-    }
-    else
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        Console.WriteLine("Такого числа в массиве нет");
+        sum = sum + array[i,j];
     }
+    Console.Write($"{sum / array.GetLength(0)} ");
+}
